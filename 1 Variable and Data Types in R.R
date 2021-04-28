3fa820a94057d5dbd3eaa93b34be191847b93bc4
Nakul-Mali <72310123+Nakul-Mali@users.noreply.github.com>
2021-04-28 20:15:11 +0530

    Add files via upload

---
+++ 1 Variable and Data Types in R.R
@@ -0,0 +1,41 @@
+
+#Integer
+
+x <- 2L
+typeof(x)
+
+
+#Doule
+y <- 2.5
+typeof(y)
+
+
+#Complex
+z <- 3+2i
+typeof(z)
+
+
+#Rewriting integer variale x
+x <- 2
+typeof(x)
+
+
+#Character
+a <- 'n'
+typeof(a)
+
+b <- "m"
+typeof(b)
+
+
+#String using character
+c <- "Nakul"
+typeof(c)
+
+
+#Logical Variable
+q1 <- T
+typeof(q1)
+
+q2 <- t
+typeof(q2)
