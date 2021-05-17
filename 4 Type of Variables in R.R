405f24c183b2ca52fd3986887794d3ccd5a35875
Nakul-Mali <72310123+Nakul-Mali@users.noreply.github.com>
2021-05-17 21:06:53 +0530

    Add files via upload

---
+++ 4 Type of Variables in R.R
@@ -0,0 +1,30 @@
+
+# Integer
+
+x <- 2L
+typeof(x) 
+
+# Double
+y <- 2.5
+typeof(y)
+
+# Complex
+z <- 3+2i
+typeof(z)
+
+# Character
+a <- 'H'
+typeof(a)
+
+b <- 'Nakul'
+typeof(b)
+
+# Logical
+c <- T
+typeof(c)
+
+d <- F
+typeof(d)
+
+
+
