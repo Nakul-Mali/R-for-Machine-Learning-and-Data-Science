aebac2c25e56f2717a749058b7e9614516755c14
Nakul-Mali <72310123+Nakul-Mali@users.noreply.github.com>
2021-05-17 21:07:21 +0530

    Add files via upload

---
+++ 5 Using Variables.R
@@ -0,0 +1,23 @@
+
+a <- 10
+b <- 5
+
+c <- a+b
+print(c)
+
+var1 <- 2.5
+var2 <- 4
+result <- var1+var2
+result
+
+# Using Squareroot function in R 
+answer <- sqrt(var2)
+answer
+
+# Combining two words by using function in R 
+gretting <- 'Hello'
+name <- 'Nakul' 
+measssage <- paste(gretting,name)
+measssage
+
+
