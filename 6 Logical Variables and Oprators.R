590562b2089c6747b474432026e35cf2e766815e
Nakul-Mali <72310123+Nakul-Mali@users.noreply.github.com>
2021-05-18 12:02:17 +0530

    Add files via upload

---
+++ 6 Logical Variables and Oprators.R
@@ -0,0 +1,46 @@
+
+# LOgical 
+# TRUE
+# FALSE
+
+4<5
+10 > 100 
+10 == 15
+
+# LOgical Oprators in R 
+# ==
+# !=
+# <
+# >
+# <=
+# >=
+# &
+# |
+# !
+# isTRUE(x)
+
+result <- 4<5
+result
+typeof(result)
+
+
+result1 <- !TRUE
+result1
+typeof(result1)
+
+result3 <- !(5>1)
+result3
+
+result | result1 
+
+result & result3
+
+isTRUE(5>4)
+
+isTRUE(10<2)
+
+isFALSE(10<2)
+
+isFALSE(10>2)
+
+
