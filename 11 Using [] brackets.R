4f1e820cbc8e0e988455e7d20b1ff55661a00a3e
Nakul-Mali <72310123+Nakul-Mali@users.noreply.github.com>
2021-05-20 20:27:17 +0530

    Add files via upload

---
+++ 11 Using [] brackets.R
@@ -0,0 +1,17 @@
+
+a <- c(10,20)         # combinning function
+b <- seq(20,50,5)     # Sequence function written with step
+c <- rep(23,5)        # Repeat
+
+# Access indiviual item from list
+w <- c('a','b','c','d','e')
+w
+w[1]
+w[3]
+w[-1]
+w[-3]
+w[1:3]
+w[c(1,3,5)]
+w[c(-2,-4)]
+w[-3:-5]
+w[1:2]
