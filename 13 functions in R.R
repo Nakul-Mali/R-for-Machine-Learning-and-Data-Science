4323cd9c4088f9845a9fde73b889afdd3b2b26e0
Nakul-Mali <72310123+Nakul-Mali@users.noreply.github.com>
2021-05-21 19:44:24 +0530

    Add files via upload

---
+++ 13 functions in R.R
@@ -0,0 +1,32 @@
+
+?rnorm()
+nrow()
+c()
+sqrt()
+rep()
+
+is.numeric()
+is.character()
+is.double()
+is.integer()
+
+print()
+typeof()
+class()
+
+seq()
+paste()
+
+# rnorm example
+rnorm(5)
+
+rnorm(5,10,8)
+
+# to see function details write ?seq() then we ger details
+seq(from=10,to=20,by=3)
+
+
+
+
+
+
