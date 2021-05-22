db3ea92ad62d6e7351cacca26890081a0fa691ce
Nakul-Mali <72310123+Nakul-Mali@users.noreply.github.com>
2021-05-22 21:05:12 +0530

    Add files via upload

---
+++ 14 Packages in R.R
@@ -0,0 +1,6 @@
+
+# When you want to intall packages in r then we go to packages in right bottom 
+# option other wise we can write code like as 
+
+install.packages("ggplot2")
+library(ggplot2)   # this line is used to activate the installed package
