9d2afc50875fd4f4426a13e01eeea584298ac1a0
Nakul-Mali <72310123+Nakul-Mali@users.noreply.github.com>
2021-06-02 20:47:45 +0530

    Add files via upload

---
+++ 23 dataset reding.R
@@ -0,0 +1,9 @@
+
+# Calories_consumed-> predict weight gained using calories consumed
+
+#Importing Dataset 
+
+dataset = read.csv('F://Data_set/calories_consumed.csv')
+dataset
+
+#Spliting the Dataset into Dependent and Independent Variable
